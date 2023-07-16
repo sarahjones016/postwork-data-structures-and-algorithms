c76403ba8289e44a78cea33df3717a4f010d0e83
Sarah Jones <sarahrjones8@gmail.com>
2023-07-16 12:06:27 -0400

    complete day 5

--- 01-week-1--starter-algorithms/04-day-5--find-shortest-string/javascript/find_shortest_string.js
+++ 01-week-1--starter-algorithms/04-day-5--find-shortest-string/javascript/find_shortest_string.js
@@ -1,5 +1,31 @@
 function findShortestString(arr) {
-  // type your code here
+
+  // Create an empty dictionary
+  // For of loop: Loop through the strings in the array
+  // Add each string to the empty dictionary:
+    // Value = the length of the string
+  
+  // Loop through the dictionary:
+    // Find the minimum value, and return the key (the string)
+
+    new_dict = {}
+    length_array = []
+
+    for (string of arr) {
+      new_dict[string] = string.length
+      length_array.push(string.length)
+    }
+
+    for (string of length_array) {
+      min = Math.min(...length_array)
+    }
+
+    for (key in new_dict) {
+      if (new_dict[key] === min) {
+        return key
+      }
+    }
+
 }
 
 if (require.main === module) {
