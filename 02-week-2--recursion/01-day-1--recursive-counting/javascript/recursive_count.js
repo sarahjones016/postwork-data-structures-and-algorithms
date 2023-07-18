a0fc129380e6c6b9a9e7d09b81cabeb2bff7ceea
Sarah Jones <sarahrjones8@gmail.com>
2023-07-18 12:19:46 -0400

    complete week 2, day 1

--- 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
+++ 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
@@ -1,6 +1,50 @@
 function recursiveCount(num = 0) {
-  // type your code here
+
+  if (num >= 10) {
+    return num
+  }
+
+  console.log(num)
+  recursiveCount(num + 1)
+
+  // console.log(num)
 }
+
+// Initial Call (execution incomplete, paused on recursive call):
+    // recursiveCount(0)
+// Recursive Call 1 (execution incomplete, paused on recursive call):
+    // recursiveCount(1)
+// Recursive Call 2 (execution incomplete, paused on recursive call):
+    // recursiveCount(2)
+// Recursive Call 3 (execution incomplete, paused on recursive call):
+    // recursiveCount(3)
+// Recursive Call 4 (execution incomplete, paused on recursive call):
+    // recursiveCount(4)
+// Recursive Call 5 (execution incomplete, paused on recursive call):
+    // recursiveCount(5)
+// Recursive Call 6 (execution incomplete, paused on recursive call):
+    // recursiveCount(6)
+// Recursive Call 7 (execution incomplete, paused on recursive call):
+    // recursiveCount(7)
+// Recursive Call 8 (execution incomplete, paused on recursive call):
+    // recursiveCount(8)
+// Recursive Call 9 (execution incomplete, paused on recursive call):
+    // recursiveCount(9)
+// Recursive Call 10 (execution incomplete, paused on recursive call):
+    // recursiveCount(10)
+
+// => Base case is hit because num >= 10, no more recursion!
+
+
+// JAVASCRIPT WHILE LOOP
+// let count = 0;
+
+// while (count < 10) {
+//   console.log(count);
+//   ++count;
+// }
+
+
 
 if (require.main === module) {
   recursiveCount();
@@ -11,3 +55,5 @@ module.exports = recursiveCount;
 // OPTIONAL
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+
