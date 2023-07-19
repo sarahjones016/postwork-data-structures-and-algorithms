529e5642fc4e47e445175d165ff05ccc16a1a3d3
Sarah Jones <sarahrjones8@gmail.com>
2023-07-19 12:19:27 -0400

    complete week 2, day 2

--- 02-week-2--recursion/02-day-2--recursive-search/javascript/recursive_search.js
+++ 02-week-2--recursion/02-day-2--recursive-search/javascript/recursive_search.js
@@ -1,5 +1,23 @@
 function recursiveSearch(arr, target) {
-  // type your code here
+  // count = 0
+
+  // if (arr[count] === target) {
+  //   return target
+  // }
+
+  // recursiveSearch(arr[count + 1], target)
+
+  if (arr.length === 0) {
+    return false;
+  }
+
+  if (arr[0] === target) {
+    return true;
+  }
+
+  return recursiveSearch(arr.slice(1), target);
+  // By using slice(1), we are continuing to move through the array, redefining arr[0]
+
 }
 
 if (require.main === module) {
@@ -17,3 +35,14 @@ module.exports = recursiveSearch;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// JAVASCRIPT LOOP
+// function iterativeSearch(arr, target) {
+//   for (const value of arr) {
+//     if (value === target) {
+//       return true;
+//     }
+//   }
+
+//   return false;
+// }
