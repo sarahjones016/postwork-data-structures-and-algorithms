fc7c3fcc9de457ba51ed41949630b410ba97a112
Sarah Jones <sarahrjones8@gmail.com>
2023-07-24 16:58:58 -0400

    week 3, day 1

--- 03-week-3--additional-practice/00-bonus-1--balancing-parenetheses/javascript/balancing_parentheses.js
+++ 03-week-3--additional-practice/00-bonus-1--balancing-parenetheses/javascript/balancing_parentheses.js
@@ -1,5 +1,46 @@
 function balancingParentheses(string) {
-  // type your code here
+ 
+// let missing = 0
+
+// for (let i = 0; i < string.length; i++) {
+//     if (string[i] === "(") {
+//       missing += 1
+//     } else if (string[i] === ")") {
+//       missing -= 1
+//     }
+// }
+// return Math.abs(missing)
+
+// * initialize missing to 0 (will store unmatched closing parens count)
+// * initialize openings to 0 (will store opening parens count)
+// *
+// * iterate over string:
+// *   if char == '(':
+// *     increment openings
+// *   else:
+// *     if openings is 0:
+// *       increment missing
+// *     else:
+// *       decrement openings
+// *
+// * return missing + openings
+
+  let missing = 0
+  let openings = 0
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === "(") {
+      openings++
+    } else {
+      if (openings === 0) {
+        missing++
+      } else {
+        openings--
+      }
+    }
+  }
+return missing + openings
+
 }
 
 if (require.main === module) {
