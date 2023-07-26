b01f8ab66402f7916435122f90a9976b816626b4
Sarah Jones <sarahrjones8@gmail.com>
2023-07-26 11:49:07 -0400

    week 3, day 2 - in progress

--- 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
+++ 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
@@ -1,5 +1,56 @@
 function romanNumeral(string) {
-  // type your code here
+
+  let romanSymbols = [
+    {symbol: "I", value: 1},
+    {symbol: "V", value: 5},
+    {symbol: "X", value: 10},
+    {symbol: "L", value: 50},
+    {symbol: "C", value: 100},
+    {symbol: "D", value: 500},
+    {symbol: "M", value: 1000},
+  ]
+
+  let arrNums = []
+
+  if (string.length === 1) {
+    for (object of romanSymbols) {
+      if (string === object.symbol) {
+        return object.value
+      }
+    }
+  } else {
+    for (let i = 0; i < string.length; i++) {
+      
+      for (object of romanSymbols) {
+        if (string[i] === object.symbol) {
+          // console.log(object.value)
+          arrNums.push(object.value)
+        }
+      }
+      
+    }
+  }
+
+  let total = Math.max(...arrNums)
+
+  for (let i = 0; i < arrNums.length; i++) {
+     if (i < arrNums.indexOf(total)) {
+       total -= arrNums[i]
+     } else if (i > arrNums.indexOf(total)) {
+       total += arrNums[i]
+     } else {
+      total += 0
+     }
+     
+  }
+
+console.log(arrNums)
+return total 
+
+  // set variables for the Roman symbols (1, 5, 10, 50, 100, 500, 1000)
+  // if length of string is 1, return the value of said variable
+  // if length of string is greater than 1, we want to identify the Roman symbols largest to smallest (though their location in the string DOES matter)
+  // 
 }
 
 if (require.main === module) {
